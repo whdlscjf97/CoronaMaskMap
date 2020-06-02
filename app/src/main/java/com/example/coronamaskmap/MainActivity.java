65572010b81f5ea61921b9ad8545a285ea5be41a
bwwoo <wbw220@naver.com>
2020-06-02 11:44:38 +0900

    메인 액티비티 화면 구성용 레이아웃 생성

--- app/src/main/java/com/example/coronamaskmap/MainActivity.java
+++ app/src/main/java/com/example/coronamaskmap/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.coronamaskmap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.example.coronamaskmap.ui.home.home_fragment;
+import com.example.coronamaskmap.ui.search.search_fragment;
+import com.example.coronamaskmap.ui.webView.webView_fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView; // 바텀네비게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private home_fragment home;
+    private search_fragment search;
+    private webView_fragment web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomnavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()) {
+                    case R.id.action_home:
+                        setFrag(0);
+                        break;
+                    case R.id.action_search:
+                        setFrag(1);
+                        break;
+                    case R.id.action_webView:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+        home = new home_fragment();
+        search = new search_fragment();
+        web = new webView_fragment();
+
+        setFrag(0);
+
+    }
+
+    // 프래그먼트 교체 실행문
+    private void setFrag(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, home);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, search);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, web);
+                ft.commit();
+                break;
+        }
     }
 }
