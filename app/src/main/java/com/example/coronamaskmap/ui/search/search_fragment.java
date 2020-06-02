0a35ec779ab0840006e3e1507f7a99cd2033ca0f
bwwoo <wbw220@naver.com>
2020-06-02 11:51:15 +0900

    기본 틀 완성

---
+++ app/src/main/java/com/example/coronamaskmap/ui/search/search_fragment.java
@@ -0,0 +1,24 @@
+package com.example.coronamaskmap.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.coronamaskmap.R;
+
+public class search_fragment extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.search_fragment, container, false);
+
+        return view;
+    }
+}
