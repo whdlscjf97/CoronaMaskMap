b40bc94e5a93d77aeeb4ef09905c1259618ab5aa
jgkim <kimjg2967@naver.com>
2020-06-03 17:10:13 +0900

    webView fragment 설정 완료

--- app/src/main/java/com/example/coronamaskmap/ui/webView/webView_fragment.java
+++ app/src/main/java/com/example/coronamaskmap/ui/webView/webView_fragment.java
@@ -1,9 +1,12 @@
 package com.example.coronamaskmap.ui.webView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,13 +15,24 @@ import androidx.fragment.app.Fragment;
 import com.example.coronamaskmap.R;
 
 public class webView_fragment extends Fragment {
-    View view;
+    private View view;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.webview_fragment, container, false);
 
+        WebView mWebView = view.findViewById(R.id.coronaitda);
+        WebSettings mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://coronaita.com/#/");
+
         return view;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setRetainInstance(true);
     }
 }
